ecced8c00efeab073ddb04ec748a3ff69f16b6d8
redmirx <redmirx9@gmail.com>
2022-10-21 02:26:23 +0500

    section-3

--- Components/src/Components/counter.jsx
+++ Components/src/Components/counter.jsx
@@ -11,10 +11,16 @@ class Counter extends Component {
     return (
       <React.Fragment>
         {/* <img src={this.state.imgUrl} alt="" /> */}
-        <span className="badge bg-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
