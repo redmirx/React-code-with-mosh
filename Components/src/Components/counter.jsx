af5826325b121207b55bf6bef3c7c327625d148e
redmirx <redmirx9@gmail.com>
2022-10-21 01:21:56 +0500

    section-3

--- Components/src/Components/counter.jsx
+++ Components/src/Components/counter.jsx
@@ -6,12 +6,13 @@ class Counter extends Component {
     count: 0,
     imgUrl: "https://picsum.photos/200",
   };
+
   render() {
     return (
       <React.Fragment>
-        <img src={this.state.imgUrl} alt="" />
-        <h1>{this.formatCount()}</h1>
-        <button>Increment</button>
+        {/* <img src={this.state.imgUrl} alt="" /> */}
+        <span className="badge bg-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
--- Components/src/index.js
+++ Components/src/index.js
@@ -5,6 +5,6 @@ import Counter from "./Components/counter";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Counter></Counter>
+    <Counter />
   </React.StrictMode>
 );
