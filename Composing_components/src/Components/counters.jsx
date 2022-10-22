32349fba53eaa2d19ed3218c9b1f2fa31a88c525
redmirx <redmirx9@gmail.com>
2022-10-23 02:23:49 +0500

    section-4

--- Composing_components/src/Components/counters.jsx
+++ Composing_components/src/Components/counters.jsx
@@ -19,9 +19,23 @@ class Counters extends Component {
     // console.log("event handler clicked", counterId);
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map((counter) => {
+      counter.value = 10;
+      return counter;
+    });
+    this.setState({ ...counters, counters });
+  };
+
   render() {
     return (
       <div>
+        <button
+          className="btn btn-primary btn-sm m-2"
+          onClick={this.handleReset}
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
