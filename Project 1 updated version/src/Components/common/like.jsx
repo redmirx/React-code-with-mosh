082c94e9a1599d0729d4be7a34892093728af8f6
redmirx <redmirx9@gmail.com>
2022-10-25 00:08:51 +0500

    Vidly Project updated

--- Project 1 updated version/src/Components/common/like.jsx
+++ Project 1 updated version/src/Components/common/like.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {} from "@fortawesome/free-solid-svg-icons";
-import { faHeart } from "@fortawesome/free-regular-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {} from "@fortawesome/free-solid-svg-icons";
+// import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
 class Like extends Component {
   render() {
     return (
       <React.Fragment>
         {/* <FontAwesomeIcon icon={faHeart} /> */}
-        <FontAwesomeIcon icon={faHeart} />
+        {/* <FontAwesomeIcon icon={faHeart} /> */}
+        <i class="fa fa-heart" aria-hidden="true"></i>
       </React.Fragment>
     );
   }
