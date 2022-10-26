0e98464796604599bf42c927c9048b3f4a61f756
redmirx <redmirx9@gmail.com>
2022-10-26 12:37:58 +0500

    Vidly Project

---
+++ Project 1 updated v3/src/Components/movies.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+import "bootstrap/dist/css/bootstrap.css";
+// import { Collapse } from "bootstrap";
+import Like from "./common/like";
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleLike = (movie) => {
+    // console.log("Like clicked", movie);
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
+  render() {
+    const onDeleteHandler = (movieID) => {
+      let filtedData = this.state.movies.filter(
+        (value) => value._id !== movieID
+      );
+      this.setState({ movies: filtedData });
+    };
+
+    if (this.state.movies.length === 0) {
+      return <p>There are no movies in the database.</p>;
+    }
+    return (
+      <React.Fragment>
+        <p>Showing {this.state.movies.length} movies in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id} className="">
+                <td className="badge bg-primary ">{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td> {movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => onDeleteHandler(movie._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;
