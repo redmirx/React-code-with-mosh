27d4abf2f85a3f3f231282058cdb016cb6b32dcd
redmirx <redmirx9@gmail.com>
2022-11-02 23:39:29 +0500

    section-6

--- router-app/src/components/admin/dashboard.jsx
+++ router-app/src/components/admin/dashboard.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = ({ match }) => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
+      <ul>
+        <li>
+          <Link to="/admin/posts"></Link>
+        </li>
+        <li>
+          <Link to="/admin/users"></Link>
+        </li>
+      </ul>
     </div>
   );
 };
