aa8dc8905c8a6a275e8c315e9b0a8c65660c4846
redmirx <redmirx9@gmail.com>
2022-11-09 23:12:45 +0500

    Vidly Project

--- Project 1 updated v4/src/App.jsx
+++ Project 1 updated v4/src/App.jsx
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Movies from "./Components/movies";
 import Customers from "./Components/customers";
 import Rentals from "./Components/rentals";
 import NotFound from "./Components/notFound";
 
 const App = () => {
-  const navigate = useNavigate();
   return (
     <main className="container">
       <Routes>
-        <Route path="/" element={<h1>Home page</h1>}></Route>
+        <Route path="/" element={<Movies />}></Route>
         <Route path="/movies" element={<Movies />}></Route>
-        <Route path="/customers" element={Customers}></Route>
-        <Route path="/rentals" element={Rentals}></Route>
-        <Route path="/not-found" element={NotFound}></Route>
+        <Route path="/customers" element={<Customers />}></Route>
+        <Route path="/rentals" element={<Rentals />}></Route>
+        <Route path="/not-found" element={<NotFound />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </main>
   );
