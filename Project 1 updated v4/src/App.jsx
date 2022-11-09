0bdc3d44da5b833c3096c20b35910f9dc70c1520
redmirx <redmirx9@gmail.com>
2022-11-09 23:03:00 +0500

    Vidly Project

--- Project 1 updated v4/src/App.jsx
+++ Project 1 updated v4/src/App.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Movies from "./Components/movies";
+import Customers from "./Components/customers";
+import Rentals from "./Components/rentals";
+import NotFound from "./Components/notFound";
 
-class App extends Component {
-  state = {};
-  render() {
-    return (
-      <main className="container">
-        <Movies />
-      </main>
-    );
-  }
-}
+const App = () => {
+  const navigate = useNavigate();
+  return (
+    <main className="container">
+      <Routes>
+        <Route path="/" element={<h1>Home page</h1>}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="/customers" element={Customers}></Route>
+        <Route path="/rentals" element={Rentals}></Route>
+        <Route path="/not-found" element={NotFound}></Route>
+      </Routes>
+    </main>
+  );
+};
 
 export default App;
