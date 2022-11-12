ce49d7b42d2d9bca9af5769805da8028a1782ad5
redmirx <redmirx9@gmail.com>
2022-11-12 23:59:59 +0500

    Vidly project

--- Project 1 updated v4/src/App.jsx
+++ Project 1 updated v4/src/App.jsx
@@ -1,19 +1,21 @@
-import React, { Component } from "react";
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Movies from "./Components/movies";
 import Customers from "./Components/customers";
 import Rentals from "./Components/rentals";
 import NotFound from "./Components/notFound";
+import NavBar from "./Components/navBar";
 
 const App = () => {
   return (
+    <
+      <NavBar />
     <main className="container">
       <Routes>
-        <Route path="/" element={<Movies />}></Route>
         <Route path="/movies" element={<Movies />}></Route>
         <Route path="/customers" element={<Customers />}></Route>
         <Route path="/rentals" element={<Rentals />}></Route>
-        <Route path="/not-found" element={<NotFound />}></Route>
+        <Route path="/" element={<Navigate to={"/movies"} />}></Route>
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </main>
